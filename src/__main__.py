1d907a42984ad36bca0c890426201f06bf7773e2
Zkeal <zkeal@outlook.com>
2020-11-15 19:02:41 +0100

    update_test

--- src/__main__.py
+++ src/__main__.py
@@ -1,52 +1,64 @@
-from typing import *
 from defeasible_tableau import *
-from reasoning_elements import *
 from propositional_parser import *
+import scenery_test_set
 
-initial_information: List[Proposition] = [
-    parse('Employed'),
-    parse('LessThanTenEmployees'),
-    parse('¬ReachedOldAgeInsurance'),
-    parse('¬MilitaryOfficial'),
-    parse('WorkedForAtLeastTwentySixWeeks'),
-]
-
-rules : List[Rule] = [
-    Rule(
-        parse('Employed'),
-        parse('CanMakeRequestForChange')
-    ),
-    Rule(
-        parse('Employed & LessThanTenEmployees'),
-        parse('¬CanMakeRequestForChange')
-    ),
-    Rule(
-        parse('Employed & ReachedOldAgeInsurance'),
-        parse('¬CanMakeRequestForChange')
-    ),
-    Rule(
-        parse('Employed & MilitaryOfficial'),
-        parse('¬CanMakeRequestForChange')
-    ),
-]
-
-preference = [
-    (1, 0),  # R2 > R1
-    (2, 0),  # R3 > R1
-    (3, 0),  # R4 > R1
-]
-
-tableau = Tableau(
-    initial_information,
-    rules,
-    parse('¬CanMakeRequestForChange')
-)
-
-pro, contra = tableau.evaluate()
-
-print('Pro:')
-for a in pro: print(a)
-print('Contra:')
-for a in contra: print(a)
+# initial_information: List[Proposition] = [
+#     parse('Employed'),
+#     parse('¬LessThanTenEmployees'),
+#     parse('¬ReachedOldAgeInsurance'),
+#     parse('MilitaryOfficial'),
+#     parse('WorkedForAtLeastTwentySixWeeks'),
+# ]
+#
+# rules : List[Rule] = [
+#     Rule(
+#         parse('Employed'),
+#         parse('CanMakeRequestForChange')
+#     ),
+#     Rule(
+#         parse('Employed & LessThanTenEmployees'),
+#         parse('¬CanMakeRequestForChange')
+#     ),
+#     Rule(
+#         parse('Employed & ReachedOldAgeInsurance'),
+#         parse('¬CanMakeRequestForChange')
+#     ),
+#     Rule(
+#         parse('Employed & MilitaryOfficial'),
+#         parse('¬CanMakeRequestForChange')
+#     )
+# ]
+#
+# preference = [
+#     (1, 0),  # R2 > R1
+#     (2, 0),  # R3 > R1
+#     (3, 0),  # R4 > R1
+# ]
+#
+# tableau = Tableau(
+#     initial_information,
+#     rules,
+#     parse('¬CanMakeRequestForChange')
+# )
+#
+# pro, contra = tableau.evaluate()
+#
+# print('Pro:')
+# for a in pro: print(a)
+# print('Contra:')
+# for a in contra: print(a)
 
 # print(tableau.root)
+
+if __name__ == '__main__':
+    example_1 = scenery_test_set.logic_example_1
+    tableau = Tableau(
+        example_1['initial_information'],
+        example_1['rules'],
+        parse('¬CanMakeRequestForChange')
+    )
+    pro, contra = tableau.evaluate()
+    print('Pro:')
+    for a in pro: print(a)
+    print('Contra:')
+    for a in contra: print(a)
---
+++ src/scenery_test_set.py
@@ -0,0 +1,83 @@
+from typing import *
+from defeasible_tableau import *
+from reasoning_elements import *
+from propositional_parser import *
+
+logic_example_1 = {'initial_information':
+[
+    parse('p ∨ q'),
+    parse('¬q'),
+],
+    'rules':[
+    Rule(
+        parse('p'),
+        parse('r')
+    ),
+    Rule(
+        parse('r'),
+        parse('s')
+    )
+],'preference':
+[
+    (1, 0),  # R2 > R1
+    (2, 0),  # R3 > R1
+    (3, 0),  # R4 > R1
+]
+}
+
+
+logic_example_2 = {'initial_information':
+[
+    parse('¬(p & q)'),
+    parse('r ∨ s'),
+    parse('t'),
+],
+    'rules':[
+    Rule(
+        parse('r'),
+        parse('p')
+    ),
+    Rule(
+        parse('t'),
+        parse('q')
+    )
+],'preference':
+[
+    (1, 0),  # R2 > R1
+    (2, 0),  # R3 > R1
+    (3, 0),  # R4 > R1
+]
+}
+
+scenery_example_2 = {'initial_information':
+[
+    parse('Employed'),
+    parse('¬LessThanTenEmployees'),
+    parse('¬ReachedOldAgeInsurance'),
+    parse('MilitaryOfficial'),
+    parse('WorkedForAtLeastTwentySixWeeks'),
+],
+    'rules':[
+    Rule(
+        parse('Employed'),
+        parse('CanMakeRequestForChange')
+    ),
+    Rule(
+        parse('Employed & LessThanTenEmployees'),
+        parse('¬CanMakeRequestForChange')
+    ),
+    Rule(
+        parse('Employed & ReachedOldAgeInsurance'),
+        parse('¬CanMakeRequestForChange')
+    ),
+    Rule(
+        parse('Employed & MilitaryOfficial'),
+        parse('¬CanMakeRequestForChange')
+    )
+],'preference':
+[
+    (1, 0),  # R2 > R1
+    (2, 0),  # R3 > R1
+    (3, 0),  # R4 > R1
+]
+}
