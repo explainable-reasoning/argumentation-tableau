862bb8582bc15c26eaea48263510758ff671dffc
Zkeal <zkeal@outlook.com>
2020-11-17 19:24:53 +0100

    fix_example

--- src/defeasible_tableau_test.py
+++ src/defeasible_tableau_test.py
@@ -349,9 +349,9 @@ def test_law_example_2():
     pro, contra = tableau.evaluate()
     print(pro)
     print(contra)
-    # assert str_list(pro) == [
-    #
-    # ]
-    # assert str_list(contra) == [
-    #     '({{Employed}, Employed ~> CanMakeRequestForChange)} CanMakeRequestForChange '
-    # ]
+    assert str_list(pro) == [
+
+    ]
+    assert str_list(contra) == [
+        '({{Employed}, Employed ~> CanMakeRequestForChange)} CanMakeRequestForChange ~> LEGAL_RequestedChangeWorkingHours)'
+    ]
