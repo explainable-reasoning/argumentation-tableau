831bed785087885c9e7184e048c0681ae3271475
Zkeal <zkeal@outlook.com>
2020-11-17 11:23:58 +0100

    add_test_set

--- src/scenery_test_set.py
+++ src/scenery_test_set.py
@@ -81,3 +81,66 @@ scenery_example_2 = {'initial_information':
     (3, 0),  # R4 > R1
 ]
 }
+
+
+scenery_example_3 = {'initial_information':
+[
+    parse('Employed'),
+    parse('¬LessThanTenEmployees'),
+    parse('¬ReachedOldAgeInsurance'),
+    parse('¬MilitaryOfficial'),
+    parse('WorkedForAtLeastTwentySixWeeks'),
+    parse('TimeSinceLastRequestMinOneYear'),
+    parse('RequestSubmittedInWriting'),
+    parse('¬UnforseenCircumstances'),
+    parse('DOES_RequestChangeWorkingHours'),
+    parse('DOES_RequestChangeWorkingTimes'),
+],
+    'rules':[
+    Rule(
+        parse('Employed'),
+        parse('CanMakeRequestForChange')
+    ),
+    Rule(
+        parse('Employed & LessThanTenEmployees'),
+        parse('¬CanMakeRequestForChange')
+    ),
+    Rule(
+        parse('Employed & ReachedOldAgeInsurance'),
+        parse('¬CanMakeRequestForChange')
+    ),
+    Rule(
+        parse('Employed & MilitaryOfficial'),
+        parse('¬CanMakeRequestForChange')
+    ),
+    Rule(
+        parse('Employed & UnforseenCircumstances'),
+        parse('CanMakeRequestForChange')
+    ),
+    Rule(
+        parse('CanMakeRequestForChange & DOES_RequestChangeWorkingHours'),
+        parse('LEGAL_RequestedChangeWorkingHours')
+    ),
+    Rule(
+        parse('CanMakeRequestForChange & DOES_RequestChangeWorkingHours & ¬RequestSubmittedInWriting'),
+        parse('¬LEGAL_RequestedChangeWorkingHours')
+    ),
+    Rule(
+        parse('CanMakeRequestForChange & DOES_RequestChangeWorkingHours & ¬TimeSinceLastRequestMinOneYear'),
+        parse('¬LEGAL_RequestedChangeWorkingHours')
+    ),
+    Rule(
+        parse('CanMakeRequestForChange & DOES_RequestChangeWorkingHours & ¬WorkedForAtLeastTwentySixWeeks'),
+        parse('¬LEGAL_RequestedChangeWorkingHours')
+    ),
+    Rule(
+        parse('CanMakeRequestForChange & DOES_RequestChangeWorkingHours &  UnforseenCircumstances'),
+        parse('LEGAL_RequestedChangeWorkingHours')
+    )
+],'preference':
+[
+    (1, 0),  # R2 > R1
+    (2, 0),  # R3 > R1
+    (3, 0),  # R4 > R1
+]
+}
