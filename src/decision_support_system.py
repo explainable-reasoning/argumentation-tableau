cfbd97592652dfca4a3b444902b0e7316ac96a08
David Schimmel <david.schimmel@gmx.de>
2021-01-05 22:18:42 +0100

    add preliminary business logic draft

--- src/decision_support_system.py
+++ src/decision_support_system.py
@@ -1,6 +1,5 @@
 from propositional_parser import toProposition
 from defeasible_tableau import Tableau
-import propositional_tableau
 
 class DecisionSupportSystem:
     """
@@ -12,9 +11,9 @@ class DecisionSupportSystem:
                  initial_information: Set[Proposition] = set(),
                  rules: Set[Rule] = set()
                  ):
-        self.initial_tableau =
-
-
+        self.question = question
+        self.initial_information = initial_information
+        self.rules = rules
 
     def __str__(self):
 
@@ -26,11 +25,28 @@ class DecisionSupportSystem:
         return
 
     def run(self):
+        done = false
+        results = None
+
         while not done:
-            tableau = Tableau
+            tableau = Tableau(question=self.question, initial_information=self.initial_information, rules=rules)
+            status, return_values = tableau.evaluate()
+            if status == 'known':
+                pro_arguments, contra_arguments = return_values[0], return_values[1]
+                done = true
+            elif status == 'unknown':
+                self.initial_information.add(self.get_promising_test(tests))
 
+        self.process_results(pro_arguments, contra_arguments)
 
+        return
 
+    def get_promising_tests(self, tests):
+        return [tests[0][0]]
 
-
+    def process_results(pro_arguments, contra_arguments):
+        for pro_argument in pro_arguments:
+            print(pro_argument)
+        for counter_argument in counter_arguments:
+            print(counter_argument)
         return
