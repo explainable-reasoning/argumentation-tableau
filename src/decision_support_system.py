5eca9b2c1c768d57fb76dbeeca72f6279f7f088f
David Schimmel <david.schimmel@gmx.de>
2021-01-05 20:24:51 +0100

    do not look at this commit

--- src/decision_support_system.py
+++ src/decision_support_system.py
@@ -1,22 +1,33 @@
+from propositional_parser import toProposition
+from defeasible_tableau import Tableau
+import propositional_tableau
+
 class DecisionSupportSystem:
     """
     Decision Support System
     """
 
-    def __init__(self):
+    def __init__(self,
+                 question: Proposition,
+                 initial_information: Set[Proposition] = set(),
+                 rules: Set[Rule] = set()
+                 ):
+        self.initial_tableau =
 
-        return
+
 
     def __str__(self):
 
         return
 
-    def setup(self):
+    def setup(self, proposition):
+
 
         return
 
     def run(self):
-
+        while not done:
+            tableau = Tableau
 
 
 
