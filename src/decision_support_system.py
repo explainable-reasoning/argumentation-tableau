c33f02ec411e123a0d58d334bc214d37ddb7f7fa
David Schimmel <david.schimmel@gmx.de>
2021-01-05 18:02:10 +0100

    initial commit§

---
+++ src/decision_support_system.py
@@ -0,0 +1,25 @@
+class DecisionSupportSystem:
+    """
+    Decision Support System
+    """
+
+    def __init__(self):
+
+        return
+
+    def __str__(self):
+
+        return
+
+    def setup(self):
+
+        return
+
+    def run(self):
+
+
+
+
+
+
+        return
